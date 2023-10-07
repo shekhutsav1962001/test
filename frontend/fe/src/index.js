e6aaf6cff3aa8e113d2a7d8fcfee8ba81469100f
shekhutsav1962001 <shekhutsav1962001@gmail.com>
2023-10-07 16:37:13 +0530

    Added HashRouter

--- frontend/fe/src/index.js
+++ frontend/fe/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 // toster css
 import "react-toastify/dist/ReactToastify.css";
 //bootstrap css
@@ -11,8 +11,10 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
